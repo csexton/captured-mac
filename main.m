ab3025ae4a5251019cee8565132d767c7ad24ec3
Christopher Sexton <csexton@gmail.com>
2011-01-17 23:39:52 -0500

    Implementing Roddi's Receipt Validation

--- main.m
+++ main.m
@@ -7,9 +7,23 @@
 //
 
 #import <Cocoa/Cocoa.h>
-#import "DirEvents.h"
-#import "EventsController.h"
+#import <Foundation/Foundation.h>
+#import "validatereceipt.h"
 
-int main(int argc, char *argv[]){
-    return NSApplicationMain(argc,  (const char **) argv);
+int main (int argc, const char * argv[]) {
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+	
+	// put the example receipt on the desktop (or change that path)
+	//NSString * pathToReceipt = @"~/Desktop/receipt";
+	NSString * pathToReceipt = [[[NSBundle mainBundle] bundlePath] stringByAppendingPathComponent:@"Contents/_MASReceipt/receipt"];
+
+	if (!validateReceiptAtPath(pathToReceipt)) { 
+		exit(173); 
+	}
+	
+    NSLog(@"Hello, correctly validated World!");
+	int ret = NSApplicationMain(argc,  (const char **) argv);
+    [pool drain];
+    return ret;
 }
+
