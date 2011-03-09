becf4489c0a6074e662a9acc8e174c7ed67195cc
Jorge Velázquez <jorge.velazquez@gmail.com>
2011-03-08 17:51:16 -0800

    this is a file transfer class that we will use to build scp and sftp support into captured

---
+++ filetransfer.cc
@@ -0,0 +1,109 @@
+// filetransfer.cc - provides additional methods for transferring images
+
+#include <sys/stat.h>
+#include <libgen.h>
+
+#include "filetransfer.h"
+
+unsigned int file_transfer::instance_count = 0;
+
+file_transfer::file_transfer()
+: handle(NULL)
+{
+	if (instance_count++ == 0)
+		curl_global_init(CURL_GLOBAL_ALL);
+	handle = curl_easy_init();
+}
+
+file_transfer::~file_transfer()
+{
+	curl_easy_cleanup(handle);
+	if (--instance_count == 0)
+		curl_global_cleanup();
+}
+
+int file_transfer::send_with_sftp(const std::string& username, const std::string& password, const std::string& host, const std::string& targetdir, const std::string& srcfile)
+{
+	CURLcode rc = CURLE_OK;
+	
+	// make sure we have a valid handle
+	if (handle == NULL)
+		return -1;
+	
+	// make sure we can open the file
+	FILE* fp = fopen(srcfile.c_str(), "rb");
+	if (fp == NULL)
+		return -2;
+	
+	// set username, if provided
+	if (username.length() > 0)
+	{
+		rc = curl_easy_setopt(handle, CURLOPT_USERNAME, username.c_str());
+		if (rc != CURLE_OK)
+		{
+			fclose(fp);
+			return rc;
+		}
+	}
+	
+	// set password, if provided
+	if (password.length() > 0)
+	{
+		rc = curl_easy_setopt(handle, CURLOPT_PASSWORD, password.c_str());
+		if (rc != CURLE_OK)
+		{
+			fclose(fp);
+			return rc;
+		}
+	}
+	
+	// set the host
+	std::string url = "sftp://" + host + "/" + targetdir + "/";
+	url.append("uniquefilenamehere");
+	rc = curl_easy_setopt(handle, CURLOPT_URL, url.c_str());
+	if (rc != CURLE_OK)
+	{
+		fclose(fp);
+		return rc;
+	}
+	
+	// tell libcurl we're doing an upload
+	rc = curl_easy_setopt(handle, CURLOPT_UPLOAD, 1);
+	if (rc != CURLE_OK)
+	{
+		fclose(fp);
+		return rc;
+	}
+
+	// stat the file, we'll need the file size for the request
+	struct stat st;
+	if (stat(srcfile.c_str(), &st) != 0)
+	{
+		fclose(fp);
+		return -3;
+	}
+	
+	// now set the file size for the request
+	rc = curl_easy_setopt(handle, CURLOPT_INFILESIZE, st.st_size);
+	if (rc != CURLE_OK)
+	{
+		fclose(fp);
+		return rc;
+	}
+
+	// give libcurl the file pointer so it can manage the upload
+	rc = curl_easy_setopt(handle, CURLOPT_READDATA, fp);
+	if (rc != CURLE_OK)
+	{
+		fclose(fp);
+		return rc;
+	}
+	
+	// do the file transfer
+	rc = curl_easy_perform(handle);
+	
+	// close the file
+	fclose(fp);
+	
+	return rc;
+}
