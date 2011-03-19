024035bb1df75dfc641daa791236d4b70cf05e0c
Jorge Velázquez <jorge.velazquez@gmail.com>
2011-03-19 11:26:33 -0700

    added url shortener

---
+++ UrlShortener.h
@@ -0,0 +1,18 @@
+//
+//  UrlShortener.h
+//  Captured
+//
+//  Created by Jorge Velázquez on 3/19/11.
+//  Copyright 2011 Codeography. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface UrlShortener : NSObject {
+	
+}
+
+- (NSString*)shorten:(NSString*)longUrl;
+
+@end
