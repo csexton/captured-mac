40a3aec319fa8cb53ad916df4475593becbbb7c1
Jorge Velázquez <jorge.velazquez@gmail.com>
2011-03-23 18:36:48 -0700

    refactored the hmac-sha1 stuff since we will use it in a few places

--- Utilities.h
+++ Utilities.h
@@ -22,8 +22,19 @@
 + (NSURL *)appURL;
 +(BOOL) willStartAtLogin:(NSURL *)itemURL;
 +(void) setStartAtLogin:(NSURL *)itemURL enabled:(BOOL)enabled;
-+ (NSImage*) thumbnailWithFile: (NSString*)path size:(NSSize)newSize;
-+ (void) growlError:(NSString*) str;
++(NSImage*) thumbnailWithFile: (NSString*)path size:(NSSize)newSize;
++(void) growlError:(NSString*) str;
++(NSString*) getHmacSha1:(NSString*)stringToSign secretKey:(NSString*)secretKey;
 
+@end
+
+@interface NSData(WithBase64)
+
+/**
+ * Return a base64 encoded representation of the data.
+ *
+ * @return base64 encoded representation of the data.
+ */
+- (NSString*) base64EncodedString;
 
 @end
--- Utilities.m
+++ Utilities.m
@@ -9,9 +9,9 @@
 #import "Utilities.h"
 #import "UrlShortener.h"
 #import <Growl/Growl.h>
+#import <CommonCrypto/CommonHMAC.h>
 
 @implementation Utilities
-
 
 +(NSDictionary*)screenCapturePrefs {
 	NSString *scprefspath = [NSHomeDirectory() stringByAppendingPathComponent:@"Library/Preferences/com.apple.screencapture.plist"];
@@ -205,6 +205,80 @@
                                    priority:0
                                    isSticky:NO
                                clickContext:[NSDate date]];
+}
+
++(NSString*) getHmacSha1:(NSString*)stringToSign secretKey:(NSString*)secretKey
+{
+	// create the signature
+	CCHmacContext context;
+	NSData* dataToSign = [stringToSign dataUsingEncoding:NSASCIIStringEncoding];
+	unsigned char digestRaw[CC_SHA1_DIGEST_LENGTH];
+	CCHmacInit(&context, kCCHmacAlgSHA1, [secretKey cStringUsingEncoding:NSASCIIStringEncoding], [secretKey length]);
+	CCHmacUpdate(&context, [dataToSign bytes], [dataToSign length]);
+	CCHmacFinal(&context, digestRaw);
+	NSData* digestData = [NSData dataWithBytes:digestRaw length:CC_SHA1_DIGEST_LENGTH];
+	return [digestData base64EncodedString];
+}
+
+@end
+
+static char base64EncodingTable[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+
+@implementation NSData (WithBase64)
+
+- (NSString *) base64EncodedString
+{
+	NSMutableString *result;
+	unsigned char   *raw;
+	unsigned long length;
+	short i, nCharsToWrite;
+	long cursor;
+	unsigned char inbytes[3], outbytes[4];
+	
+	length = [self length];
+	
+	if (length < 1)
+		return @"";
+	
+	result = [NSMutableString stringWithCapacity:length];
+	raw = (unsigned char *)[self bytes];
+	
+	// Take 3 chars at a time, and encode to 4
+	for (cursor = 0; cursor < length; cursor += 3) {
+		
+		for (i = 0; i < 3; i++) {
+			if (cursor + i < length) 
+				inbytes[i] = raw[cursor + i];
+			else 
+				inbytes[i] = 0;
+		}
+		
+		outbytes[0] = (inbytes[0] & 0xFC) >> 2;
+		outbytes[1] = ((inbytes[0] & 0x03) << 4) | ((inbytes[1] & 0xF0) >> 4);
+		outbytes[2] = ((inbytes[1] & 0x0F) << 2) | ((inbytes[2] & 0xC0) >> 6);
+		outbytes[3] = inbytes[2] & 0x3F;
+		
+		nCharsToWrite = 4;
+		
+		switch (length - cursor) {
+			case 1:
+				nCharsToWrite = 2;
+				break;
+			case 2:
+				nCharsToWrite = 3;
+				break;
+		}
+		
+		for (i = 0; i < nCharsToWrite; i++) {
+			[result appendFormat:@"%c", base64EncodingTable[outbytes[i]]];
+		}
+		
+		for (i = nCharsToWrite; i<4; i++) {
+			[result appendString:@"="];
+		}
+	}
+	
+	return [NSString stringWithString:result]; // convert to immutable string
 }
 
 @end
