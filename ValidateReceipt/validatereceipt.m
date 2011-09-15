fb831f4fde3d6f2744bdd1ed5a6501e6113df045
Christopher Sexton <csexton@gmail.com>
2011-09-15 13:48:15 -0400

    Version bump to 3.0

--- ValidateReceipt/validatereceipt.m
+++ ValidateReceipt/validatereceipt.m
@@ -393,7 +393,7 @@ BOOL validateReceiptAtPath(NSString * path)
 	// http://www.craftymind.com/2011/01/06/mac-app-store-hacked-how-developers-can-better-protect-themselves/
 
 	// so use hard coded values instead (probably even somehow obfuscated)
-	bundleVersion = @"2.1";
+	bundleVersion = @"3.0";
 	bundleIdentifier = @"com.codeography.captured-mac";
 
 	// avoid making stupid mistakes --> check again
